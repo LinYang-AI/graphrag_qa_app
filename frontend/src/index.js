c6e7be7886b9b9920b67a2d702266f219b3166d1
LinYang-AI <linyoung1214@outlook.com>
2025-09-28 15:51:17 +0200

    add a Q/A user interface with graph view widget

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GraphVisualization from './GraphVisualization';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<GraphVisualization />);
+root.render(<App />);
